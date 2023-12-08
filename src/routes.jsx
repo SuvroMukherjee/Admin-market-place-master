b38a04ce4ab6cec810a096f98aaa9e7d49f5d419
SuvroMukherjee <subhra.m@onenesstechs.in>
2023-12-08 11:42:24 +0530

    push

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,44 +1,39 @@
 import { useRoutes } from 'react-router-dom';
 import AdminLayout from './Layouts/AdminLayout';
 import LoginPage from './Login/LoginPage';
-import Home from './Pages/Home/Home';
-import {Roles} from './Pages/Roles/Roles';
-import UserList from './Pages/UserList/UserList';
-import Topbar from './components/Topbar/Topbar';
-import useAuth from './hooks/useAuth';
 import AddUser from './Pages/AddUser/AddUser';
-
+import Home from './Pages/Home/Home';
+import { Roles } from './Pages/Roles/Roles';
+import UserList from './Pages/UserList/UserList';
+import useAuth from './hooks/useAuth';
+import RequireAuth from './components/RequireAuth/RequireAuth'
 
 
 export default function Router() {
 
-    const { auth } = useAuth();
-
-    let allRoutes = [];
-
-    if (!auth) {
-        allRoutes = [
-            {
-                path: '/',
-                element: <LoginPage />,
-            },
-        ]
-    } else {
-        allRoutes = [
-            {
-                path: '/',
-                element: <AdminLayout />,
-                children: [
-                    { path: 'AdminDashboard', element: <Home /> },
-                    { path: 'roles', element: <Roles /> },
-                    { path: 'users', element: <UserList /> },
-                    { path: 'AddUser', element: <AddUser /> },
-
-                ]
-            },
-        ]
-    }
-
+  
+    const allRoutes = [
+        {
+            path: '/',
+            element: <AdminLayout />,
+            children: [
+                {
+                    path: '/',
+                    element: <RequireAuth allowedRoles={['656d6fca298f781cbdd844bd']} />,
+                    children: [
+                        { path: 'AdminDashboard', element: <Home /> },
+                        { path: 'roles', element: <Roles /> },
+                        { path: 'users', element: <UserList /> },
+                        { path: 'AddUser', element: <AddUser /> },
+                    ],
+                },
+            ],
+        },
+        {
+            path: '/login',
+            element: <LoginPage />,
+        },
+    ];
 
 
 
