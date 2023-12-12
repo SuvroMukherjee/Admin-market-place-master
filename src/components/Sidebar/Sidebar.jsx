acc912bc5be2ada53aa2e3340c2f173769b059b2
SuvroMukherjee <subhra.m@onenesstechs.in>
2023-12-13 01:45:39 +0530

    Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,6 +9,7 @@ import './Navbar.css';
 import { SidebarData } from './SidebarData';
 import "./sidebar.css";
 import { LuUnlock } from "react-icons/lu";
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 export default function Sidebar() {
     const { auth, logout } = useAuth();
@@ -18,103 +19,6 @@ export default function Sidebar() {
     const showSidebar = () => setSidebar(!sidebar);
 
     return (
-        // <div className="sidebar" >
-        //     <div className="sidebarWrapper">
-        //         <div className="sidebarMenu">
-        //             <h3 className="sidebarTitle">Admin Dashboard</h3>
-        //             <hr style={{ color: 'white' }} />
-        //             {/* <ul className="sidebarList">
-        //                 <Link to="/" className="link">
-        //                     <li className="sidebarListItem active">
-        //                         <LineStyle className="sidebarIcon" />
-        //                         Home
-        //                     </li>
-        //                 </Link>
-        //                 <Link to="/roles" className="link">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Roles
-        //                     </li>
-        //                 </Link>
-        //                 <Link to="/users" className="link">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Users List
-        //                     </li>
-        //                 </Link>
-        //                 <Link to="/AddUser" className="link">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Add User
-        //                     </li>
-        //                 </Link>
-        //                 <Link to={'/'} className="link" onClick={() => logout()}>
-        //                     <li className="sidebarListItem">
-        //                         <WorkOutline className="sidebarIcon" />
-        //                         Logout
-        //                     </li>
-        //                 </Link>
-        //             </ul> */}
-
-        //             <ul className="sidebarList">
-        //                 <NavLink exact to="/" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <LineStyle className="sidebarIcon" />
-        //                         Home
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink to="/roles" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Roles
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink to="/users" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Users 
-        //                     </li>
-        //                 </NavLink>
-        //                 {/* <NavLink to="/AddUser" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Add User
-        //                     </li>
-        //                 </NavLink> */}
-        //                 <NavLink to="/Admin/category" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Category 
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink to="/Admin/subcategory" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Sub Category 
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink to="/Admin/brand" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                         Brand
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink to="/Admin/product" className="link" activeClassName="active">
-        //                     <li className="sidebarListItem">
-        //                         <PermIdentity className="sidebarIcon" />
-        //                          Product
-        //                     </li>
-        //                 </NavLink>
-        //                 <NavLink exact to="/" className="link" onClick={() => logout()}>
-        //                     <li className="sidebarListItem">
-        //                         <WorkOutline className="sidebarIcon" />
-        //                         Logout
-        //                     </li>
-        //                 </NavLink>
-        //             </ul>
-        //         </div>
-        //         </div>
-        // </div>
         <div className="sidebar">
             <IconContext.Provider value={{ color: '#fff' }}>
                 {/* <div className='navbar'>
@@ -129,7 +33,7 @@ export default function Sidebar() {
                         </li>
                         {SidebarData.map((item, index) => {
                             return (
-                                <li key={index} className={item.cName}>
+                                <li key={index} className={location.pathname === item.path ? 'nav-text-active' : 'nav-text'}>
                                     <Link to={item.path}>
                                         {item.icon}
                                         <span>{item.title}</span>
