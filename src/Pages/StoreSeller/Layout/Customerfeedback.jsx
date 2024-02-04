9ad94bfa1ac51e60b615f6a5a6bbd1d862ae736c
SuvroMukherjee <subhra.m@onenesstechs.in>
2024-02-04 13:27:46 +0530

    p

--- src/Pages/StoreSeller/Layout/Customerfeedback.jsx
+++ src/Pages/StoreSeller/Layout/Customerfeedback.jsx
@@ -4,13 +4,17 @@ import { Col, Container, Pagination, Row, Table } from 'react-bootstrap';
 import { FaStar } from "react-icons/fa";
 import { SlCalender } from "react-icons/sl";
 import { SellerProductList } from '../../../API/api';
+import calender from '../../../assets/calendar.png'
+import user from '../../../assets/user.png'
+import placeholder from '../../../assets/placeholder.png'
 
 const Customerfeedback = () => {
 
     const { userId } = JSON.parse(localStorage.getItem('auth'));
 
     const [productData,setProductData] = useState();
-    const [reviewData,setReviewData] = useState([])
+    const [reviewData,setReviewData] = useState([]);
+    const [orderdata,setOrderdata] = useState()
 
     useEffect(() => {
         SellingProducts()
@@ -22,6 +26,7 @@ const Customerfeedback = () => {
             setProductData(res?.data?.data?.SellerProductData)
             setReviewData(res?.data?.data?.reviewData)
             // CalculateAvgRating(res?.data?.data?.SellerProductData, res?.data?.data?.reviewData)
+            setOrderdata(res?.data?.data?.orderData)
         }).catch((err) => {
             console.log(err)
         }).finally((data) => {
@@ -47,6 +52,22 @@ const Customerfeedback = () => {
         setCurrentPage(pageNumber);
     };
 
+
+    const findOrderdate = (id) =>{
+        // let filter = orderdata?.map((ele) => {
+        //     // Filter out order_details array based on sellerId
+        //     ele.order_details = ele.order_details?.filter((item) => {
+        //         if (item.proId?.available_qty == 48){
+        //             alert('call',item)
+        //         }
+        //     });
+
+        //     return ele;
+        // }).filter(ele => ele.order_details.length > 0); // Remove objects with empty order_details array
+
+        // console.log({filter})
+    }
+
     return (
         <div>
             <Container className='mt-4'>
@@ -57,37 +78,37 @@ const Customerfeedback = () => {
                 </Row>
                 <Row className='mt-4'>
                     <Col>
-                        <Table striped bordered hover responsive>
+                        <Table  responsive>
                             <thead>
                                 <tr>
-                                    <th>No</th>
+                                   
                                     <th>Customer Feedback</th>
                                     <th>Customer</th>
                                     <th>Rating</th>
                                     <th>product</th>
                                     <th>SKU</th>
-                                    <th>Delivery date</th>
-                                    <th>Visit on Site</th>
+                                    {/* <th>Delivery date</th>
+                                    <th>Visit on Site</th> */}
                                 </tr>
                             </thead>
                             <tbody>
                                 {currentItems?.length ? currentItems.map((row,index) => (
-                                    <tr key={row.id}>
-                                        <td>{index+1}</td>
-                                        <td> 
+                                    <tr key={row.id} className='cusfeedtd'>
+                                       
+                                        <td className='cusfeedtd'> 
                                             <div>
                                                 <h6>{row?.title}</h6>
-                                                <p>{row?.desc}</p>
-                                                <p><SlCalender/> {moment(row?.createdAt).fromNow()}</p>
+                                                <p className='cusfeedtd' style={{width:'450px'}}>{row?.desc}</p>
+                                                <p className='cusfeedtd'><img src={calender} width={20}/>  {moment(row?.createdAt).fromNow()}</p>
                                             </div>
                                             </td>
                                         <td>
                                            <div>
-                                             <p>{row?.user?.name}</p>
-                                                <p>{JSON.parse(row?.user?.address)?.state}</p>
+                                            <p><img src={user}/> {row?.user?.name}</p>
+                                                <p><img src={placeholder} width={15}/> {JSON.parse(row?.user?.address)?.state}</p>
                                            </div>
                                         </td>
-                                        <td><FaStar /> {row?.rating}</td>
+                                        <td><FaStar size={30} color='gold'/> {row?.rating}</td>
                                         <td>{row?.proId?.name} {row?.proId?.specId?.spec_det?.length > 0 && (
                                             <span>
                                                 (
@@ -101,8 +122,8 @@ const Customerfeedback = () => {
                                             </span>
                                         )}</td>
                                         <td>{row?.proId?.specId?.skuId}</td>
-                                        <td>{row?.proId?.specId?.skuId}</td>
-                                        <td>{row?.proId?.specId?.skuId}</td>
+                                        {/* <td>{row?.proId?.specId?.skuId} {findOrderdate(row?.proId?._id)} </td>
+                                        <td>{row?.proId?.specId?.skuId}</td> */}
                                     </tr>
                                 )) : <div>No Feedback Yet</div>}
                             </tbody>
