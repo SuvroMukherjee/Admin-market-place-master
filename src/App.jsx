4ab7c9d60424d5c34a55b5326c1edd5fa24fd854
SuvroMukherjee <subhra.m@onenesstechs.in>
2024-04-08 21:04:24 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 // App.js
-import React from 'react';
+import React,{useEffect} from 'react';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { AuthProvider } from './context/auth';
 import Router from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import io from 'socket.io-client';
 
+const socket = io('http://localhost:5000'); // Adjust URL accordingly
 
 function App() {
+
+  useEffect(() => {
+        // Listen for notifications from the server
+        socket.on('notification', (message) => {
+            console.log('Notification from server:', message);
+            // You can handle the notification as needed in your React app
+        });
+
+        return () => {
+            // Cleanup function to remove event listener when component unmounts
+            socket.off('notification');
+        };
+    }, []);
+    
   return (
     <>
       <AuthProvider>
