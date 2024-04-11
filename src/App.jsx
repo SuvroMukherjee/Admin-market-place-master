5b93983bf7b20fed866f51056a21a4ca14c0f9eb
irahuldutta04 <rahul.dutta@techmantra.co>
2024-04-11 16:54:54 +0530

    feat: admin notification ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 // App.js
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import { AuthProvider } from "./context/auth";
 import Router from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import io from 'socket.io-client';
-import { io } from 'socket.io-client';
-import toast, { Toaster } from 'react-hot-toast';
-import useAuth from './hooks/useAuth';
-
-
+import { io } from "socket.io-client";
+import toast, { Toaster } from "react-hot-toast";
+import useAuth from "./hooks/useAuth";
 
 function App() {
-
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
   const [socket, setSocket] = useState(null);
@@ -29,19 +26,29 @@ function App() {
   }, [socket, user]);
 
   const send = () => {
-    console.log('Sending notification...');
+    console.log("Sending notification...");
     socket.emit("sendNotification", {
       senderName: "user",
       receiverName: "marketplace.admin@gmail.com",
       type: "1000",
     });
-  }
+  };
 
   return (
     <>
       <AuthProvider>
-        <button onClick={()=>send()} style={{height:'50vh',width:'800px',background:'blue',color:'white'}}>Click</button>
-        <Router    socket = {socket } />
+        <button
+          onClick={() => send()}
+          style={{
+            height: "50vh",
+            width: "800px",
+            background: "blue",
+            color: "white",
+          }}
+        >
+          Click
+        </button>
+        <Router socket={socket} />
       </AuthProvider>
     </>
   );
