a41c8b47f9c1150fc6087ece61150601c7d154bd
irahuldutta04 <rahul.dutta@techmantra.co>
2024-04-16 17:13:48 +0530

    fix: remove logs

--- src/common/RatingAvg.js
+++ src/common/RatingAvg.js
@@ -1,62 +1,40 @@
 export const ratingCalculation = (id, reviewData) => {
+  let filterReview = reviewData?.filter((item) => item?.proId?._id == id);
 
-    console.log({reviewData})
+  if (!filterReview || filterReview.length === 0) {
+    return 0;
+  }
+  const sumOfRatings = filterReview.reduce((total, review) => {
+    return total + parseInt(review.rating);
+  }, 0);
 
-    let filterReview = reviewData?.filter((item) => (
-        item?.proId?._id == id
-    ));
-     console.log({ filterReview });
-
-    if (!filterReview || filterReview.length === 0) {
-        return 0;
-    }
-    const sumOfRatings = filterReview.reduce((total, review) => {
-        return total + parseInt(review.rating);
-    }, 0);
-
-    const averageRating = sumOfRatings / filterReview.length;
-    console.log({averageRating})
-    return averageRating;
+  const averageRating = sumOfRatings / filterReview.length;
+  return averageRating;
 };
 
 export function addOrdinalSuffix(number) {
-    if (typeof number !== 'number') {
-        return 'Input is not a number';
-    }
+  if (typeof number !== "number") {
+    return "Input is not a number";
+  }
 
-    if (number === 11 || number === 12 || number === 13) {
-        return number + 'th';
-    }
+  if (number === 11 || number === 12 || number === 13) {
+    return number + "th";
+  }
 
-    var lastDigit = number % 10;
-    var suffix = 'th';
+  var lastDigit = number % 10;
+  var suffix = "th";
 
-    switch (lastDigit) {
-        case 1:
-            suffix = 'st';
-            break;
-        case 2:
-            suffix = 'nd';
-            break;
-        case 3:
-            suffix = 'rd';
-            break;
-    }
+  switch (lastDigit) {
+    case 1:
+      suffix = "st";
+      break;
+    case 2:
+      suffix = "nd";
+      break;
+    case 3:
+      suffix = "rd";
+      break;
+  }
 
-    return number + suffix;
+  return number + suffix;
 }
-
-// {
-//     row?.proId?.specId?.spec_det?.length > 0 && (
-//         <span>
-//             (
-//             {row?.proId?.specId?.spec_det.map((ele, index, array) => (
-//                 <span key={index}>
-//                     {ele.value}
-//                     {index < array.length - 1 ? ', ' : ''}
-//                 </span>
-//             ))}
-//             )
-//         </span>
-//     )
-// }
