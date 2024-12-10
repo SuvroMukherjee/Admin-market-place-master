75aabce50481f74e304286cbc2564763f1015b67
SuvroMukherjee <subhra.m@techmantra.co>
2024-12-10 13:28:28 +0530

    push

--- src/Pages/StoreSeller/NewProductAddition/NewAddLayout.jsx
+++ src/Pages/StoreSeller/NewProductAddition/NewAddLayout.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import "./newproduct.css";
+import { IoArrowBackCircle } from "react-icons/io5";
 
 const NewAddLayout = () => {
   const SellerNewProductId = localStorage.getItem("Seller-productId") || "";
+
+  const navigate = useNavigate();
 
   return (
     <div>
       <Container className="mt-4">
         <Row className="p-1 justify-content-md-center">
+          <Col xs={2}>
+            <Button
+              size="sm"
+              variant="outline-dark"
+              onClick={() => navigate("/seller/seller-request")}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "0.5rem",
+              }}
+            >
+              <IoArrowBackCircle size={20} />
+              <span>Back</span>
+            </Button>
+          </Col>
           <Col xs={10}>
             <ul className="d-flex justify-content-evenly liststyle">
               <li>
